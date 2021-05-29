8b7a97baba55a501f7808837b21480b60b00ad04
Josh Aragon <josharagon99@gmail.com>
2021-05-28 22:44:20 -0600

    Reduce page height to prevent scroll

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,7 +8,9 @@ const SearchBar = () => {
   return (
     <form>
       <input type='search' placeholder='Find a GitHub repository'></input>
-      <button type='submit'><img src={searchGlass}></img></button>
+      <button type='submit' onClick={(e) => {
+        e.preventDefault()
+        console.log('BRUH')}}><img src={searchGlass}></img></button>
     </form>
   )
 }
